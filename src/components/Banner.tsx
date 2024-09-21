fa95eab45454bf0fb6e2e00f663f3ecea8f7babe
naimur-reza <naimur.rezaa@gmail.com>
2024-09-21 13:00:39 +0600

    Added mobile menu & fixed responsive issues

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -7,9 +7,9 @@ const Banner = () => {
   return (
     <>
       <div className="  flex items-center justify-center min-h-[calc(100vh+80px)]">
-        <div className="grid container py-20 grid-cols-12 items-center ">
-          <div className="  p-4  col-span-6">
-            <h1 className="text-5xl font-bold">
+        <div className="grid container text-center lg:text-left py-20 grid-cols-12 items-center ">
+          <div className="  py-20  col-span-12 lg:col-span-6">
+            <h1 className="lg:text-5xl text-3xl  font-bold">
               Find the Next Crypto Gem on{" "}
               <span className="text-blue-500 underline">
                 Crito<span className="text-yellow-500 underline">X</span>
@@ -19,7 +19,7 @@ const Banner = () => {
               Coin CritoX is the easiest, safest, and fastest way to buy & sell
               crypto asset exchange.
             </p>
-            <div className="flex gap-4">
+            <div className="flex gap-4 justify-center lg:justify-start">
               <div className="border border-blue-500 group  hover:border-white rounded-full transition-all duration-500 p-0.5 w-fit ">
                 <button className="btn-primary">Connect Wallet</button>
               </div>
@@ -28,9 +28,9 @@ const Banner = () => {
               </div>
             </div>
           </div>
-          <div className="  p-4 col-span-12 lg:col-span-5 lg:col-start-8 relative max-lg:flex max-lg:justify-center ">
-            <img className="relative z-10" src={hero} alt="Hero" />
-            <div className="w-[280px] h-[280px] md:w-[350px] md:h-[350px] absolute top-[6%] blur-[85px] left-0 bg-[#3a96ff]/50"></div>
+          <div className="  lg:p-4 col-span-12 lg:col-span-5 lg:col-start-8 relative max-lg:flex max-lg:justify-center ">
+            <img className="relative z-[3]" src={hero} alt="Hero" />
+            <div className="w-[280px] h-[280px] md:w-[350px] md:h-[350px] absolute top-[6%] blur-[85px] left-0 bg-[#3a96ff]/50" />
           </div>
         </div>
         <div className="max-lg:hidden w-[250px] xxl:w-[350px] h-[250px] xxl:h-[350px] absolute top-[6%] blur-[85px] left-[-12%] bg-[#3a96ff]/50" />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,41 @@
 import React from "react";
 import logo from "../assets/icons/logo.png";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const menuItems = [
+    {
+      name: "Buy Crypto",
+      link: "/buy-crypto",
+    },
+    {
+      name: "Sell Crypto",
+      link: "/sell-crypto",
+    },
+    {
+      name: "Swap",
+      link: "/swap",
+    },
+    {
+      name: "Coin Market",
+      link: "/coin-market",
+    },
+    {
+      name: "Earn",
+      link: "/earn",
+    },
+    {
+      name: "Pages",
+      link: "/pages",
+    },
+  ];
+
   return (
     <div className=" z-10 py-4 xxl:py-6 border-b border-gray-800 fixed top-0 left-0 right-0 w-full  bg-black">
       <div className="container flex items-center justify-between">
@@ -9,31 +43,65 @@ const Navbar = () => {
           <img src={logo} alt="logo" />
         </div>
         <div className="navbar-menu">
-          <ul className="flex items-center gap-4  font-medium">
-            <li>Buy Crypto</li>
-            <li>Sell Crypto</li>
-            <li>Swap</li>
-            <li>Coin Market</li>
-            <li>Earn</li>
-            <li className="relative group">
-              <button className="lg:text-lg py-2 inline-flex items-center gap-1 submenu-btn">
-                Pages
-              </button>
-              <ul className="absolute hidden group-hover:block bg-black p-10 left-0 top-full ">
-                <li>Pages</li>
-                <li>Pages</li>
-                <li>Pages</li>
-                <li>Pages</li>
-                <li>Pages</li>
-                <li>Pages</li>
-                <li>Pages</li>
-              </ul>
-            </li>
+          <ul className="hidden lg:flex items-center gap-4  font-medium">
+            {menuItems.map((item, index) => (
+              <li key={index}>
+                <a href={item.link}>{item.name}</a>
+              </li>
+            ))}
           </ul>
         </div>
 
-        <div className="navbar-actions border border-blue-500 group  hover:border-white rounded-full transition-all duration-500 p-0.5 w-fit ">
+        <div className="navbar-actions border border-blue-500 group  hover:border-white rounded-full ml-auto mr-2 lg:m-0 transition-all duration-500 p-0.5 w-fit ">
           <button className="btn-primary">Connect Wallet</button>
+        </div>
+
+        {/* mobile menu */}
+        {/* Sidebar Button for Mobile */}
+        <button onClick={toggleSidebar} className="lg:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="icon icon-tabler icon-tabler-menu-2"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M4 6l16 0" />
+            <path d="M4 12l16 0" />
+            <path d="M4 18l16 0" />
+          </svg>
+        </button>
+
+        {/* Sidebar */}
+        <div
+          className={`fixed  top-0 left-0 h-full w-[250px] bg-gray-900 transform ${
+            isOpen ? "translate-x-0" : "-translate-x-full"
+          } transition-transform duration-300 ease-in-out z-50`}
+        >
+          <div className="flex justify-between items-center p-4 border-b border-gray-700">
+            <div className="navbar-logo">
+              <img src={logo} alt="logo" />
+            </div>
+            <button
+              onClick={toggleSidebar}
+              className="text-white text-2xl focus:outline-none"
+            >
+              &times;
+            </button>
+          </div>
+          <nav className="flex flex-col p-4 space-y-3">
+            {menuItems.map((item, index) => (
+              <a key={index} href={item.link}>
+                {item.name}
+              </a>
+            ))}
+          </nav>
         </div>
       </div>
     </div>
